67ee4ae96d8cd31f28b0c3b72467f07309313dc9
Stephan Sure <stephan.sure@gmail.com>
2024-02-24 15:31:19 +0000

    Add name check of the player with data validation

--- run.py
+++ run.py
@@ -9,4 +9,13 @@ tprint("---Extended---",font="hyper",chr_ignore=True)
 
 print('Welcome to Rock-Paper-Scissors Extended!\n')
 
+user_name = input("Please enter your Name:")
+# The strip() method ensures that something has to be entered and the isalpha() method ensures that no numbers are entered 
+while not user_name.strip() or not user_name.isalpha():
+    print("The text field must not be left blank and only letters are permitted!")
+    user_name = input("Please enter your Name:")
+print()
+print(f"{user_name} nice to have you here. This is an extension of the classic game Rock-Paper-Scissors.\n"
+"Compete against the computer and test your luck!\n"
+)
 

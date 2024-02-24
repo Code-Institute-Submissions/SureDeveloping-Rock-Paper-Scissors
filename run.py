0c62b745bc35f34399749fe9ee24f50cbfca7dd1
Stephan Sure <stephan.sure@gmail.com>
2024-02-24 15:25:20 +0000

    Add Game title and welcome text.

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+from art import *
+
+tprint("Rock-Paper-Scissors",font="clb6x10",chr_ignore=True,)
+tprint("---Extended---",font="hyper",chr_ignore=True)
+
+print('Welcome to Rock-Paper-Scissors Extended!\n')
+
+
